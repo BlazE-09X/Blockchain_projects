6dc5ea4f849701e6198be2229d8cf13ec702f746
BlazE-09X <aruzansaparbek172@gmail.com>
2026-04-22 18:57:22 +0000

    Deploying to gh-pages from @ BlazE-09X/Blockchain_projects@21a8fb48b485e39897c55dbb83e7ce2be23751a4 🚀

---
+++ project7/ballot-next/_next/static/O-B9hsDXuWjESAPyJoIzJ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Blockchain_projects/project7/ballot-next//_next/:path+",
+        "destination": "/Blockchain_projects/project7/ballot-next/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
